156a042111e19f96de279f67993892988720d1d8
Leaf-moon <68877012+Leaf-moon@users.noreply.github.com>
2022-08-15 19:04:26 +0300

    Create home.component.ts

---
+++ frontend/src/app/modules/general/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core'; 
+ import { environment } from '../../../../environments/environment'; 
+ import { SeoService } from '../../../services/seo/seo.service'; 
+  
+ @Component({ 
+   selector: 'app-home', 
+   templateUrl: './home.component.html', 
+   styleUrls: ['./home.component.css'] 
+ }) 
+ export class HomeComponent { 
+  
+   name = environment.application.name; 
+   angular = environment.application.angular; 
+   bootstrap = environment.application.bootstrap; 
+   fontawesome = environment.application.fontawesome; 
+  
+   constructor(private seoService: SeoService) { 
+  
+     const content = 
+       'This application was developed with ' + this.angular + ' and ' + this.bootstrap + 
+       ' It applies Routing, Lazy loading and Progressive Web App (PWA)'; 
+  
+     const title = 'angular-starter Title : Home Page'; 
+  
+     this.seoService.setMetaDescription(content); 
+     this.seoService.setMetaTitle(title); 
+  
+  
+   } 
+  
+ }
