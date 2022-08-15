15aa6c3adfec5c9265e529c07cf5c958af54243f
Leaf-moon <68877012+Leaf-moon@users.noreply.github.com>
2022-08-15 18:47:02 +0300

    Create app.module.ts

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser'; 
+ import { NgModule } from '@angular/core'; 
+  
+ import { AppComponent } from './app.component'; 
+ import { HomeComponent } from './modules/general/home/home.component'; 
+ import { NotFoundComponent } from './modules/general/not-found/not-found.component'; 
+ import { AppRoutingModule } from './app-routing.module'; 
+ import { HttpClientModule } from '@angular/common/http'; 
+  
+ import { HeaderModule } from './components/header/header.module'; 
+ import { FooterModule } from './components/footer/footer.module'; 
+  
+ @NgModule({ 
+   declarations: [ 
+     AppComponent, 
+     HomeComponent, 
+     NotFoundComponent, 
+   ], 
+   imports: [ 
+     BrowserModule, 
+     AppRoutingModule, 
+     HttpClientModule, 
+     HeaderModule, 
+     FooterModule, 
+   ], 
+   providers: [], 
+   bootstrap: [AppComponent] 
+ }) 
+ export class AppModule { }
